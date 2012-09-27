3126795f55fcede89c96e0ad6f84c137a76a8eef
Andy Sonnenburg <andy22286@gmail.com>
2012-09-27 16:45:37 -0500

    more explicit export list to improve haddock output

--- Control/Monad/Ref.hs
+++ Control/Monad/Ref.hs
@@ -6,11 +6,14 @@ Portability: non-portable
 -}
 module Control.Monad.Ref
        ( module Exports
+       , MonadRef (..)
+       , modifyRefDefault
+       , MonadAtomicRef (..)
        ) where
 
 import Control.Monad as Exports
 import Control.Monad.Fix as Exports
 import Control.Monad.Instances ()
 import Control.Monad.IO.Class as Exports
-import Control.Monad.Ref.Class as Exports
+import Control.Monad.Ref.Class
 import Control.Monad.Trans.Class as Exports
